9db4f80f46b30cad8780a79496d3307ad760eca3
Czcibor Bohusz-Dobosz <czbd@o2.pl>
2024-02-09 05:43:21 +0100

    fix: compare significant digits in tests

--- spec/atmospheric/isa_spec.rb
+++ spec/atmospheric/isa_spec.rb
@@ -4,28 +4,28 @@ require "yaml"
 
 RSpec.describe Atmospheric::Isa do
   mapping_var_to_method_name = {
-    # method name, digits to round
+    # method name, decimal places or nil, significant digits or nil
     H: nil,
-    h: ["geometric_altitude_from_geopotential", 0],
-    TK: ["temperature_at_layer_from_geopotential", 3],
-    TC: ["temperature_at_layer_celcius", 3],
-    p_mbar: ["pressure_from_geopotential_mbar", 2],
-    p_mmhg: ["pressure_from_geopotential_mmhg", 3],
-    rho: ["density_from_geopotential", 5],
-    g: ["gravity_at_geopotential", 4],
-    p_p_n: ["p_p_n_from_geopotential", 5],
-    rho_rho_n: ["rho_rho_n_from_geopotential", 5],
-    root_rho_rho_n: ["root_rho_rho_n_from_geopotential", 5],
-    a: ["speed_of_sound_from_geopotential", 3],
-    mu: ["dynamic_viscosity_from_geopotential", 9],
-    v: ["kinematic_viscosity_from_geopotential", 9],
-    lambda: ["thermal_conductivity_from_geopotential", 6],
-    H_p: ["pressure_scale_height_from_geopotential", 1],
-    gamma: ["specific_weight_from_geopotential", 3],
-    n: ["air_number_density_from_geopotential", -21],
-    v_bar: ["mean_air_particle_speed_from_geopotential", 2],
-    omega: ["air_particle_collision_frequency_from_geopotential", -5],
-    l: ["mean_free_path_of_air_particles_from_geopotential", 12],
+    h: ["geometric_altitude_from_geopotential", 0, nil],
+    TK: ["temperature_at_layer_from_geopotential", 3, nil],
+    TC: ["temperature_at_layer_celcius", 3, nil],
+    p_mbar: ["pressure_from_geopotential_mbar", nil, 6],
+    p_mmhg: ["pressure_from_geopotential_mmhg", nil, 6],
+    rho: ["density_from_geopotential", nil, 6],
+    g: ["gravity_at_geopotential", 4, nil],
+    p_p_n: ["p_p_n_from_geopotential", nil, 6],
+    rho_rho_n: ["rho_rho_n_from_geopotential", nil, 6],
+    root_rho_rho_n: ["root_rho_rho_n_from_geopotential", nil, 6],
+    a: ["speed_of_sound_from_geopotential", 3, nil],
+    mu: ["dynamic_viscosity_from_geopotential", nil, 5],
+    v: ["kinematic_viscosity_from_geopotential", nil, 5],
+    lambda: ["thermal_conductivity_from_geopotential", nil, 5],
+    H_p: ["pressure_scale_height_from_geopotential", 1, nil],
+    gamma: ["specific_weight_from_geopotential", nil, 5],
+    n: ["air_number_density_from_geopotential", nil, 5],
+    v_bar: ["mean_air_particle_speed_from_geopotential", 2, nil],
+    omega: ["air_particle_collision_frequency_from_geopotential", nil, 5],
+    l: ["mean_free_path_of_air_particles_from_geopotential", nil, 5],
   }.freeze
 
   let(:isa) { Atmospheric::Isa }
@@ -39,10 +39,14 @@ RSpec.describe Atmospheric::Isa do
 
       it "conforms to test values at H(#{hash['H']}), variable (#{var})" do
         method_name = method[0]
-        method_round = method[1]
+        decimal_places = method[1]
+        significant_digits = method[2]
 
         expected_value = hash[var.to_s]
-        calc = isa.send(method_name, geopotential_h).round(method_round)
+        calc = isa.send(method_name, geopotential_h)
+        calc = calc.round(decimal_places) if !decimal_places.nil?
+        calc = calc.round(significant_digits - Math.log10(calc).ceil) \
+		  if !significant_digits.nil? and calc != 0
 
         # For variable :n, the calculated value is an integer, but the tests
         # have it as a float, so we need to convert the calculated value to
