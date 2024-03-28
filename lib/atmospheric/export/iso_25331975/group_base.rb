71c61fb3dcf15700e797abf1ab7331f4117b833a
Ronald Tse <ronald.tse@ribose.com>
2024-03-28 14:55:24 +0800

    fix: table value grouping

--- lib/atmospheric/export/iso_25331975/group_base.rb
+++ lib/atmospheric/export/iso_25331975/group_base.rb
@@ -43,8 +43,8 @@ module Atmospheric
         # Step 50 from -2k to 40k, step 100 above 32k, 200 above 51k to 80k
         def steps
           (
-            (-2000..31999).step(50) +
-            (32000..50999).step(100) +
+            (-2000..31950).step(50) +
+            (32000..50900).step(100) +
             (51000..80000).step(200)
           )
         end
@@ -56,7 +56,7 @@ module Atmospheric
         def to_h(unit: steps_unit)
           d = {
             "by-geometrical-altitude" => [],
-            "by-geopotential-altitude" => []
+            "by-geopotential-altitude" => [],
           }
 
           steps.each do |h|
