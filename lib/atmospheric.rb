ea4b57aec7a6f82931db5ea0a507073c2710ce11
0arm0 <arehman0279@gmail.com>
2024-12-10 22:57:36 +0500

    Add adapters

--- lib/atmospheric.rb
+++ lib/atmospheric.rb
@@ -3,7 +3,16 @@
 require_relative "atmospheric/version"
 require_relative "atmospheric/isa"
 require_relative "atmospheric/export"
+require 'lutaml/model'
+require 'lutaml/model/xml_adapter/nokogiri_adapter'
+require 'lutaml/model/json_adapter/standard_json_adapter'
+require 'lutaml/model/yaml_adapter/standard_yaml_adapter'
 
+Lutaml::Model::Config.configure do |config|
+  config.xml_adapter = Lutaml::Model::XmlAdapter::NokogiriAdapter
+  config.yaml_adapter = Lutaml::Model::YamlAdapter::StandardYamlAdapter
+  config.json_adapter = Lutaml::Model::JsonAdapter::StandardJsonAdapter
+end
 module Atmospheric
   class Error < StandardError; end
   # Your code goes here...
