7d372f332aa17488e0ffbc6ed2b79202227be702
0arm0 <arehman0279@gmail.com>
2024-12-10 23:02:53 +0500

    Add attrs class for GroupOne

---
+++ lib/atmospheric/export/iso_25331975/group_one_attrs.rb
@@ -0,0 +1,33 @@
+require 'lutaml/model'
+
+module Atmospheric
+  module Export
+    module Iso25331975
+      class GroupOneAttrs < Lutaml::Model::Serializable
+        attribute :geometrical_altitude_m, :integer
+        attribute :geometrical_altitude_ft, :integer
+        attribute :geopotential_altitude_m, :integer
+        attribute :geopotential_altitude_ft, :integer
+        attribute :temperature_k, :integer
+        attribute :temperature_c, :integer
+        attribute :pressure_mbar, :decimal
+        attribute :pressure_mmhg, :decimal
+        attribute :density, :decimal
+        attribute :acceleration, :float
+
+        key_value do
+          map "geometrical-altitude-m", to: :geometrical_altitude_m
+          map "geometrical-altitude-ft", to: :geometrical_altitude_ft
+          map "geopotential-altitude-m", to: :geopotential_altitude_m
+          map "geopotential-altitude-ft", to: :geopotential_altitude_ft
+          map "temperature-K", to: :temperature_k
+          map "temperature-C", to: :temperature_c
+          map "pressure-mbar", to: :pressure_mbar
+          map "pressure-mmhg", to: :pressure_mmhg
+          map "density", to: :density
+          map "acceleration", to: :acceleration
+        end
+      end
+    end
+  end
+end
