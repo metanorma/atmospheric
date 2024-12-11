3d1d57aebcca8db15f9dcc9c6901af6b4d42c5d1
0arm0 <arehman0279@gmail.com>
2024-12-11 18:14:09 +0500

    Add specs for iso25331975

--- lib/atmospheric/export/iso_25331975/group_three_attrs.rb
+++ lib/atmospheric/export/iso_25331975/group_three_attrs.rb
@@ -1,19 +1,25 @@
-require 'lutaml/model'
+require "lutaml/model"
 
 module Atmospheric
   module Export
     module Iso25331975
       class GroupThreeAttrs < Lutaml::Model::Serializable
-        attribute :geometrical_altitude, :integer
-        attribute :pressure_scale_height, :decimal
-        attribute :specific_weight, :decimal
-        attribute :air_number_density, :decimal
+        attribute :geometrical_altitude_m, :integer
+        attribute :geometrical_altitude_ft, :integer
+        attribute :geopotential_altitude_m, :integer
+        attribute :geopotential_altitude_ft, :integer
+        attribute :pressure_scale_height, :float
+        attribute :specific_weight, :float
+        attribute :air_number_density, :float
         attribute :mean_speed, :float
-        attribute :frequency, :decimal
-        attribute :mean_free_path, :decimal
+        attribute :frequency, :float
+        attribute :mean_free_path, :float
 
-        yaml do
-          map "geometrical-altitude", to: :geometrical_altitude
+        key_value do
+          map "geometrical-altitude-m", to: :geometrical_altitude_m
+          map "geometrical-altitude-ft", to: :geometrical_altitude_ft
+          map "geopotential-altitude-m", to: :geopotential_altitude_m
+          map "geopotential-altitude-ft", to: :geopotential_altitude_ft
           map "pressure-scale-height", to: :pressure_scale_height
           map "specific-weight", to: :specific_weight
           map "air-number-density", to: :air_number_density
--- lib/atmospheric/export/iso_25331975/group_two_attrs.rb
+++ lib/atmospheric/export/iso_25331975/group_two_attrs.rb
@@ -1,20 +1,26 @@
-require 'lutaml/model'
+require "lutaml/model"
 
 module Atmospheric
   module Export
     module Iso25331975
       class GroupTwoAttrs < Lutaml::Model::Serializable
-        attribute :geometrical_altitude, :integer
-        attribute :ppn, :decimal
-        attribute :rhorhon, :decimal
-        attribute :sqrt_rhorhon, :decimal
+        attribute :geometrical_altitude_m, :integer
+        attribute :geometrical_altitude_ft, :integer
+        attribute :geopotential_altitude_m, :integer
+        attribute :geopotential_altitude_ft, :integer
+        attribute :ppn, :float
+        attribute :rhorhon, :float
+        attribute :sqrt_rhorhon, :float
         attribute :speed_of_sound, :integer
-        attribute :dynamic_viscosity, :decimal
-        attribute :kinematic_viscosity, :decimal
-        attribute :thermal_conductivity, :decimal
+        attribute :dynamic_viscosity, :float
+        attribute :kinematic_viscosity, :float
+        attribute :thermal_conductivity, :float
 
-        yaml do
-          map "geometrical-altitude", to: :geometrical_altitude
+        key_value do
+          map "geometrical-altitude-m", to: :geometrical_altitude_m
+          map "geometrical-altitude-ft", to: :geometrical_altitude_ft
+          map "geopotential-altitude-m", to: :geopotential_altitude_m
+          map "geopotential-altitude-ft", to: :geopotential_altitude_ft
           map "ppn", to: :ppn
           map "rhorhon", to: :rhorhon
           map "sqrt-rhorhon", to: :sqrt_rhorhon
