df55ca81c5a16d00f3ccb15f375e7ee7767f64d9
0arm0 <arehman0279@gmail.com>
2024-12-11 16:42:49 +0500

    feat: migrate iso-25332024 to LutaML

---
+++ lib/atmospheric/export/iso_25332024/hypsometrical_geopotential_attrs.rb
@@ -0,0 +1,17 @@
+require 'lutaml/model'
+
+module Atmospheric
+  module Export
+    module Iso25332024
+      class HypsometricalGeopotentialAttrs < Lutaml::Model::Serializable
+        attribute :geopotential_altitude_m, :integer
+        attribute :pressure_mbar, :float
+
+        key_value do
+          map "geopotential-altitude-m", to: :geopotential_altitude_m
+          map "pressure-mbar", to: :pressure_mbar
+        end
+      end
+    end
+  end
+end
