7765259f82e7735a95e44638213457ee1ef3f310
0arm0 <arehman0279@gmail.com>
2024-12-11 18:35:54 +0500

    Added specs for iso-25332024

---
+++ spec/atmospheric/export/iso_25332024_spec.rb
@@ -0,0 +1,64 @@
+require "spec_helper"
+require_relative "../../../lib/atmospheric/export/iso_25332024"
+
+RSpec.describe "Iso25332024 Parsing and Serialization" do
+  let(:table_5_yaml_path) { "spec/fixtures/iso2533/sources/iso-2533-2024/yaml/table5.yaml" }
+  let(:table_6_yaml_path) { "spec/fixtures/iso2533/sources/iso-2533-2024/yaml/table6.yaml" }
+  let(:table_7_yaml_path) { "spec/fixtures/iso2533/sources/iso-2533-2024/yaml/table7.yaml" }
+  let(:table_8_yaml_path) { "spec/fixtures/iso2533/sources/iso-2533-2024/yaml/table8.yaml" }
+  let(:table_9_yaml_path) { "spec/fixtures/iso2533/sources/iso-2533-2024/yaml/table9.yaml" }
+  let(:table_10_yaml_path) { "spec/fixtures/iso2533/sources/iso-2533-2024/yaml/table10.yaml" }
+  let(:table_11_yaml_path) { "spec/fixtures/iso2533/sources/iso-2533-2024/yaml/table11.yaml" }
+  let(:table_12_yaml_path) { "spec/fixtures/iso2533/sources/iso-2533-2024/yaml/table12.yaml" }
+  let(:table_13_yaml_path) { "spec/fixtures/iso2533/sources/iso-2533-2024/yaml/table13.yaml" }
+
+  def verify_yaml_round_trip(yaml_path, model_class)
+    # Read the original YAML file
+    original_yaml = File.read(yaml_path)
+
+    # Parse the YAML content using the model's from_yaml method
+    model_instance = model_class.from_yaml(original_yaml)
+
+    # Convert the object back to YAML
+    generated_yaml = model_instance.to_yaml
+
+    # Ensure the generated YAML matches the original YAML
+    expect(YAML.load(generated_yaml.strip)).to eq(YAML.load original_yaml.strip)
+  end
+
+  it 'correctly parses and serializes Table 5 YAML' do
+    verify_yaml_round_trip(table_5_yaml_path, Atmospheric::Export::Iso25332024::GroupOneMeters)
+  end
+
+  it 'correctly parses and serializes Table 6 YAML' do
+    verify_yaml_round_trip(table_6_yaml_path, Atmospheric::Export::Iso25332024::GroupTwoMeters)
+  end
+
+  it 'correctly parses and serializes Table 7 YAML' do
+    verify_yaml_round_trip(table_7_yaml_path, Atmospheric::Export::Iso25332024::GroupThreeMeters)
+  end
+
+  it 'correctly parses and serializes Table 8 YAML' do
+    verify_yaml_round_trip(table_8_yaml_path, Atmospheric::Export::Iso25332024::GroupOneFeet)
+  end
+
+  it 'correctly parses and serializes Table 9 YAML' do
+    verify_yaml_round_trip(table_9_yaml_path, Atmospheric::Export::Iso25332024::GroupTwoFeet)
+  end
+
+  it 'correctly parses and serializes Table 10 YAML' do
+    verify_yaml_round_trip(table_10_yaml_path, Atmospheric::Export::Iso25332024::GroupThreeFeet)
+  end
+
+  it 'correctly parses and serializes Table 11 YAML' do
+    verify_yaml_round_trip(table_11_yaml_path, Atmospheric::Export::Iso25332024::HypsometricalMbar)
+  end
+
+  it 'correctly parses and serializes Table 12 YAML' do
+    verify_yaml_round_trip(table_12_yaml_path, Atmospheric::Export::Iso25332024::HypsometricalGeometric)
+  end
+
+  it 'correctly parses and serializes Table 13 YAML' do
+    verify_yaml_round_trip(table_13_yaml_path, Atmospheric::Export::Iso25332024::HypsometricalGeopotential)
+  end
+end
