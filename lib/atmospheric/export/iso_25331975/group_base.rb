c369ce1f7668a1dc5a8541ddf5c15522a39b8f14
Ronald Tse <ronald.tse@ribose.com>
2025-02-07 18:37:46 +0800

    chore: move tests from private repo without submodule

--- lib/atmospheric/export/iso_25331975/group_base.rb
+++ lib/atmospheric/export/iso_25331975/group_base.rb
@@ -43,11 +43,9 @@ module Atmospheric
 
         # Step 50 from -2k to 40k, step 100 above 32k, 200 above 51k to 80k
         def steps
-          (
-            (-2000..31950).step(50) +
-            (32000..50900).step(100) +
-            (51000..80000).step(200)
-          )
+          ((-2000..31950).step(50) +
+           (32000..50900).step(100) +
+           (51000..80000).step(200))
         end
 
         def steps_unit
